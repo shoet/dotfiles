1c0c7e5dc297129000c267ddd3201a6659c0b1a6
shoet <32211937+shoet@users.noreply.github.com>
2023-11-18 20:07:59 +0900

    Cheetsheet追加

---
+++ memo/cheet.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+nvim ${HOME}/memo/cheetsheet.md
--- memo/setup.sh
+++ memo/setup.sh
@@ -10,4 +10,8 @@ if [ ! -e ${HOME}/tools/memo ]; then
     ln -s ${CUR_DIR}/memo.sh ${HOME}/tools/memo
 fi
 
+if [ ! -e ${HOME}/tools/cheet ]; then
+    ln -s ${CUR_DIR}/cheet.sh ${HOME}/tools/cheet
+fi
+
 echo "Please add 'export PATH=\$PATH:\${HOME}/tools'"
